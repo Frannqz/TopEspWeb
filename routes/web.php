82d6d6f48754dc0bf35474c0c4b8885e10ce5f21
Francisco Gonzalez <paco_glez02@hotmail.com>
2024-02-11 21:59:03 -0600

    Groceries controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GroceriesController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\siteController; // Importa el controlador
 
@@ -14,7 +15,13 @@ use App\Http\Controllers\siteController; // Importa el controlador
 |
 */
 
-Route::get('/', [siteController::class, 'index'])->name("home"); // name("home") es el nombre de la ruta
-Route::get('/serv', [siteController::class, 'services'])->name("services");
-Route::get('/products', [siteController::class, 'products'])->name("products");
-Route::get('/contact', [siteController::class, 'contact'])->name("contact");
+// Route::get('/', [siteController::class, 'index'])->name("home"); // name("home") es el nombre de la ruta
+// Route::get('/serv', [siteController::class, 'services'])->name("services");
+// Route::get('/products', [siteController::class, 'products'])->name("products");
+// Route::get('/contact', [siteController::class, 'contact'])->name("contact");
+
+
+Route::get('/', [GroceriesController::class, 'index'])->name("home"); // name("home")
+Route::get('/store/shop', [GroceriesController::class, 'shop'])->name("shop"); // name("home")
+Route::get('/register', [GroceriesController::class, 'register'])->name("register"); // name("home")
+Route::get('/login', [GroceriesController::class, 'login'])->name("login"); // name("home")
