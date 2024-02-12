f50f1a5dc25ac7b1de1fc96f5fc499f394b32124
Francisco Gonzalez <paco_glez02@hotmail.com>
2024-02-11 22:52:47 -0600

    pendiente login

--- resources/views/groceries/login.blade.php
+++ resources/views/groceries/login.blade.php
@@ -30,7 +30,8 @@
                                         <input id="checkbox0" type="checkbox" name="remember">
                                         <label for="checkbox0" class="mb-0"> Remember Me? </label>
                                     </div>
-                                    <a href="login.html" class="text-light"><i class="fa fa-bell"></i> Forgot password?</a>
+                                    <a href="{{ route('login') }}" class="text-light"><i class="fa fa-bell"></i> Forgot
+                                        password?</a>
                                 </div>
                             </div>
                             <div class="form-group row text-center mt-4">
