216f73ab1b14b4da71853c611a8e9fc852411dcc
Francisco Gonzalez <paco_glez02@hotmail.com>
2024-02-13 19:27:02 -0600

    Iniciando conexion a BD pgsql

--- app/Http/Controllers/GroceriesController.php
+++ app/Http/Controllers/GroceriesController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class GroceriesController extends Controller
 {
     public function index() {
+        $products = Product::all();
+
+        //dd($products);
+        echo "<pre>";
+        print_r($products);
+        echo "</pre>";
+        die();
+
         return view("groceries.index");
     }
     public function shop() {
