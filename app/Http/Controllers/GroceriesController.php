30c3cf06254d9946aa51ec75aadabc696c6a09f5
Francisco Gonzalez <paco_glez02@hotmail.com>
2024-02-14 12:50:32 -0600

    Obtencion de datos desde BD

--- app/Http/Controllers/GroceriesController.php
+++ app/Http/Controllers/GroceriesController.php
@@ -2,24 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class GroceriesController extends Controller
 {
     public function index() {
-        $products = Product::all();
-
+        //$products = Product::all();
         //dd($products);
-        echo "<pre>";
-        print_r($products);
-        echo "</pre>";
-        die();
+        // echo "<pre>";
+        // print_r($products);
+        // echo "</pre>";
+        // die();
 
         return view("groceries.index");
     }
     public function shop() {
-        return view("groceries.shop");
+        $products = Product::all();
+        $categories = Category::all();
+        return view("groceries.shop", compact("products","categories"));
     }
     public function register() {
         return view("groceries.register");
